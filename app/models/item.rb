f2f3f9ee4eb15901b5caf842704c2bd98f324281
hiddy0329 <hiddy19970329@gmail.com>
2022-04-25 16:14:03 +0900

    モデルのバリデーションの記述変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,21 @@
 class Item < ApplicationRecord
   validates :name,             presence: true
   validates :description,      presence: true
-  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
-  validates :state_id,         numericality: { other_than: 1, message: "can't be blank" }
-  validates :postage_id,       numericality: { other_than: 1, message: "can't be blank" }
-  validates :region_id,        numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id,      numericality: { other_than: 1, message: "を選択してください" }
+  validates :state_id,         numericality: { other_than: 1, message: "を選択してください" }
+  validates :postage_id,       numericality: { other_than: 1, message: "を選択してください" }
+  validates :region_id,        numericality: { other_than: 1, message: "を選択してください" }
+  validates :shipping_date_id, numericality: { other_than: 1, message: "を選択してください" }
 
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank" }
+                      presence: { message: "を入力してください" }
   end
   
   has_one :order
   belongs_to :user
   has_one_attached :image
-  validates :image, presence: true
+  validates :image, presence: { message: "を選択してください" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,13 +3,13 @@ class OrderAddress
   attr_accessor :postal_code, :shipping_prefecture_id, :shipping_municipality, :shipping_address, :building_name, :phone_number, :item_id, :user_id, :token
   
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "をハイフン(-)を含めて正しく入力してください"}
     validates :shipping_municipality
     validates :shipping_address
     validates :phone_number,format: { with: /\A[0-9]{10,11}\z/}
     validates :token
   end
-  validates :shipping_prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipping_prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
   validates :user_id, presence: {message: "must exist"}
   validates :item_id, presence: {message: "must exist"}
   
