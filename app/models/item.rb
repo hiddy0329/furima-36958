6110e652dd712342a7eb9b76205e93d593e29d49
hiddy0329 <hiddy19970329@gmail.com>
2022-05-02 17:30:31 +0900

    Itemモデルのバリデーションの記述をItemFormクラスに移動

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,7 @@
 class Item < ApplicationRecord
-  validates :name,             presence: true
-  validates :description,      presence: true
-  validates :category_id,      numericality: { other_than: 1, message: "を選択してください" }
-  validates :state_id,         numericality: { other_than: 1, message: "を選択してください" }
-  validates :postage_id,       numericality: { other_than: 1, message: "を選択してください" }
-  validates :region_id,        numericality: { other_than: 1, message: "を選択してください" }
-  validates :shipping_date_id, numericality: { other_than: 1, message: "を選択してください" }
-
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "を入力してください" }
-  end
-  
   has_one :order
   belongs_to :user
   has_many_attached :images
-  validates :images, presence: { message: "を選択してください" }
-  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
