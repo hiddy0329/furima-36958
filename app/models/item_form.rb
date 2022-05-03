64b163769f8c362e6ea34499a46a9f6b339a3aff
hiddy0329 <hiddy19970329@gmail.com>
2022-05-03 16:59:26 +0900

    ItemFormクラスにてupdateメソッドを更新

--- app/models/item_form.rb
+++ app/models/item_form.rb
@@ -36,6 +36,20 @@ class ItemForm
   end
 
   def update(params, item)
+    #一度タグの紐付けを消すために中間テーブルの内容を削除
+    item.item_tag_relations.destroy_all
+
+    #paramsの中のタグの情報を削除。同時に、返り値として新たなタグの情報を変数に代入
+    tag_name = params.delete(:tag_name)
+
+    #もしタグの情報がすでに保存されていればインスタンスを取得、無ければインスタンスを新規作成
+    tag = Tag.where(tag_name: tag_name).first_or_initialize if tag_name.present?
+
+    #タグを保存
+    tag.save if tag_name.present?
+    #商品を保存
     item.update(params)
+    #中間テーブルに保存
+    ItemTagRelation.create(item_id: item.id, tag_id: tag.id) if tag_name.present?
   end
 end
