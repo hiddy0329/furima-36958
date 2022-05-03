326258aee5052ef71afb4f8056bf6c99ff6f06f0
hiddy0329 <hiddy19970329@gmail.com>
2022-05-03 16:12:11 +0900

    アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_many :comments
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
