224ff8ce9463fc043d2503645ea73dfdf0a9c838
hiddy0329 <hiddy19970329@gmail.com>
2022-05-06 18:33:00 +0900

    タグ入力フォームの取得有無を確認

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,1 +1,6 @@
-console.log("読み込み完了");
+document.addEventListener("DOMContentLoaded", () => {
+const tagNameInput = document.querySelector("#item_form_tag_name");
+  if (tagNameInput){
+    console.log("読み込み完了");
+  };
+});
