873c70abf4174a29b9ad5897d5859f4b10e82b6d
hiddy0329 <hiddy19970329@gmail.com>
2022-05-09 17:11:18 +0900

    逐次検索機能用のパスを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :create]
     resources :comments, only: :create
     collection do
+      get 'inclimental_search'
+    end
+    collection do
       get 'search'
     end
   end
